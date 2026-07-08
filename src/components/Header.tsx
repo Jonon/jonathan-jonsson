d41bee4382379f1a29419108b6303d165927792d
Jonathan <jonathancjonsson@gmail.com>
2026-07-08 16:31:20 +0200

    feat(header): Create header with title and menu icon

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { IoMenuSharp } from "react-icons/io5";
+import "./header.css";
+
+export default function Header() {
+	return (
+		<div className="container">
+			<h1 className="title">Jonathan Jonsson</h1>
+			<IoMenuSharp className="IoMenuSharp" />
+		</div>
+	);
+}
