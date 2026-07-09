b619611b3f6786ce7c41e63cdde4cc6d72afd918
Jonathan <jonathancjonsson@gmail.com>
2026-07-09 16:47:02 +0200

    feat: Create desktop navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,25 @@ import { IoMenuSharp } from "react-icons/io5";
 import "./header.css";
 
 export default function Header() {
+	const { width } = useWindowDimensions();
+
+	const list = [
+		{ label: "Home" },
+		{ label: "About" },
+		{ label: "Projects" },
+		{ label: "Contact" },
+	];
+
+	const MenuList = (
+		<nav className="navigation">
+			<ul>
+				{list.map((listItem) => {
+					return <li>{listItem.label.toUpperCase()}</li>;
+				})}
+			</ul>
+		</nav>
+	);
+
 	return (
 		<div className="container">
 			<h1 className="title">Jonathan Jonsson</h1>
