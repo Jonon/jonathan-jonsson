9fe4dec255b9b62036bd8d537a33df9bc8e7cb8c
Jonathan <jonathancjonsson@gmail.com>
2026-07-14 22:09:23 +0200

    feat(ui): Create drop down mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { IoMenuSharp } from "react-icons/io5";
+import { IoCloseSharp } from "react-icons/io5";
 import "./header.css";
 import useWindowDimensions from "../hooks/useWindowDimensions";
 
@@ -22,10 +23,33 @@ export default function Header() {
 		</nav>
 	);
 
+	const MobileMenuList = (
+		<nav className="mobile-navigation">
+			<ul>
+				{list.map((listItem) => {
+					return <li>{listItem.label.toUpperCase()}</li>;
+				})}
+			</ul>
+		</nav>
+	);
+
 	return (
-		<div className="container">
-			<h1 className="title">Jonathan Jonsson</h1>
-			{width > 960 ? MenuList : <IoMenuSharp className="icon icon-menu" />}
-		</div>
+		<>
+			<div className="container">
+				<h1 className="title">Jonathan Jonsson</h1>
+				{width > 960 ? (
+					MenuList
+				) : (
+					<IoMenuSharp
+						onClick={() => setDropdown(!showDropdown)}
+						className="icon icon-menu"
+					/>
+				)}
+			</div>
+			<div className="dropdown-menu">
+				<IoCloseSharp className="icon icon-close" />
+				{MobileMenuList}
+			</div>
+		</>
 	);
 }
