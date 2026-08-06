76419271c22361a68f36218d1a9a64efe377a56c
Jonathan <jonathancjonsson@gmail.com>
2026-08-06 14:00:30 +0200

    refactor: Remove none existing css class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -69,7 +69,7 @@ export default function Header() {
 					/>
 				)}
 			</div>
-			<div className={isDropdownOpen ? "overlay" : "overlay-disable"}></div>
+			<div className={isDropdownOpen ? "overlay" : ""}></div>
 			<div
 				className={isDropdownOpen ? "dropdown-menu" : "dropdown-menu-display"}
 			>
