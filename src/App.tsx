88155aac1fa49db452f010c493a6d2edf4f1d60f
Jonathan <jonathancjonsson@gmail.com>
2026-09-02 12:14:09 +0200

    feat: add skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
+import Skills from "./components/Skills";
 
 export default function App() {
 	return (
@@ -9,6 +10,7 @@ export default function App() {
 			<main className="main">
 				<Intro />
 				<About />
+				<Skills />
 			</main>
 		</>
 	);
