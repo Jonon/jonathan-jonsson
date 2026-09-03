3e6b17d1f6d4a58c35bce231cf99cdaadc48690f
Jonathan <jonathancjonsson@gmail.com>
2026-09-03 18:53:47 +0200

    feat: Create reusable button with icon

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,45 @@
 import styles from "./button.module.css";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+import type { IconType } from "react-icons";
 
-export default function button(props: {
+const icons: Record<IconName, IconType> = {
+	github: FaGithub,
+	externalLink: FaExternalLinkAlt,
+};
+
+interface Button {
 	name: string;
-	variant: "primary" | "secondary";
-}) {
-	const handleVariant = () =>
-		props.variant === "primary"
-			? styles.primary
-			: props.variant === "secondary"
-				? styles.secondary
-				: "";
+	variant: "primary" | "secondary" | "tertiary";
+	icon?: IconName;
+	style?: React.CSSProperties;
+}
+
+type IconName = "github" | "externalLink";
+
+export default function button({ name, variant, icon, style }: Button) {
+	const Icon = icon ? icons[icon] : null;
+	const handleVariant = () => {
+		switch (variant) {
+			case "primary":
+				return styles.primary;
+			case "secondary":
+				return styles.secondary;
+			case "tertiary":
+				return styles.tertiary;
+			default:
+				return "";
+		}
+	};
 
 	return (
-		<button className={`${styles.btn} ${handleVariant()}`}>{props.name}</button>
+		<button
+			type="button"
+			className={`${styles.btn} ${handleVariant()}`}
+			style={style}
+		>
+			{variant === "tertiary" && Icon ? <Icon className={styles.icon} /> : null}
+
+			{name}
+		</button>
 	);
 }
