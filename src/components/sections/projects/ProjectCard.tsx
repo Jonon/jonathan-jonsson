874c25a24773432b6052d550adbc5068f99681db
Jonathan <jonathancjonsson@gmail.com>
2026-09-11 18:43:41 +0200

    feat: add buttons with dynamic data

--- src/components/sections/projects/ProjectCard.tsx
+++ src/components/sections/projects/ProjectCard.tsx
@@ -6,9 +6,17 @@ interface Project {
 	title: string;
 	summary: string;
 	languages: Array<ReactNode>;
+	github: string;
+	website: string;
 }
 
-export default function ProjectCard({ title, languages, summary }: Project) {
+export default function ProjectCard({
+	title,
+	languages,
+	summary,
+	github,
+	website,
+}: Project) {
 	return (
 		<div className={styles.projectCard}>
 			<h3>{title}</h3>
@@ -21,15 +29,29 @@ export default function ProjectCard({ title, languages, summary }: Project) {
 				<p>{summary}</p>
 			</div>
 			<div className={styles.buttons}>
-				<Button name="CODE" variant="tertiary" icon="github" />
-				<Button
-					name="OPEN"
-					variant="tertiary"
-					icon="externalLink"
-					style={{
-						backgroundColor: "var(--color-accent-600)",
-					}}
-				/>
+				<a
+					href={github}
+					target="_blank"
+					rel="noopener noreferrer"
+					style={{ textDecoration: "none" }}
+				>
+					<Button name="CODE" variant="tertiary" icon="github" />
+				</a>
+				<a
+					href={website}
+					target="_blank"
+					rel="noopener noreferrer"
+					style={{ textDecoration: "none" }}
+				>
+					<Button
+						name="OPEN"
+						variant="tertiary"
+						icon="externalLink"
+						style={{
+							backgroundColor: "var(--color-accent-600)",
+						}}
+					/>
+				</a>
 			</div>
 		</div>
 	);
